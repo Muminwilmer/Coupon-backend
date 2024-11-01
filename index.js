b36f2c5dfc9c6263af1355b6f3f532471e15a969
Aglomation <wilmer.heltborg2008@gmail.com>
2024-11-01 04:41:15 +0100

    added new page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const fs = require('fs')
 
 // Middleware to parse JSON bodies from requests
@@ -15,17 +15,17 @@ app.post('/qualifioapp/', async (req, res) => {
   console.log(req.body)
   console.log('Content-Type:', req.headers['content-type']);
 
-  const domain = req.body.domain;
+  const domain = req.body.domain.replace(/\S+\./, "");
   if (!domain) res.status(400).json({ message: "You need to add the domain"});
   const pageUrl = req.body.pageUrl
   if (!pageUrl) res.status(400).json({ message: "You need to add the pageUrl"});
   const formData = req.body.formData
   if (!formData) res.status(400).json({ message: "You need to add the formData"});
-
+  const extraPage = req.body.extraPage || ""
 
   try {
     const automizeProcess = require("./public/qualifioapp/index.js")
-    const results = await automizeProcess(formData, domain, pageUrl)
+    const results = await automizeProcess(formData, domain, pageUrl, extraPage)
     const [id] = results
 
     // Send success response
@@ -38,12 +38,11 @@ app.post('/qualifioapp/', async (req, res) => {
 
 app.get('/404*', (req, res, next) => {
   fs.readdir(path.join(__dirname, 'public', '404'), (err, files) => {
-    // Filter for .jpg or .png files (or whatever formats you have)
     const catImages = files.filter(file => 
       file.endsWith('.jpg') || file.endsWith('.png')
     );
 
-    // Select a random cat image
+    // Select a random 404 cat image
     const randomIndex = Math.floor(Math.random() * catImages.length);
     const randomCatImage = catImages[randomIndex];
     console.log(randomCatImage)
--- public/qualifioapp/index.js
+++ public/qualifioapp/index.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-async function fetchAndExtractID(domain, pageUrl) {
+async function fetchAndExtractID(domain, pageUrl, extraPage) {
   try {
-    const response = await axios.get(`https://${domain}.se/${pageUrl}`)
+    const response = await axios.get(`https://${domain}.se/${extraPage}${pageUrl}`)
 
     const match = response.data.match(/'([A-Z0-9-]{36})'/);
     if (match && match[1]) {
@@ -12,7 +12,11 @@ async function fetchAndExtractID(domain, pageUrl) {
       throw new Error('No ID was found??');
     }
   } catch (error) {
-    console.error('Error fetching ID:', error);
+    // console.error('Error fetching ID:', error);
+    console.log("Error!")
+    console.log("domain", domain)
+    console.log("pageUrl", pageUrl)
+    console.log("extraPage", extraPage)
   }
 }
 
@@ -29,7 +33,10 @@ async function followRedirects(id, domain) {
     const response = await axios.get(url, { maxRedirects: 5 });
     return response.request.res.responseUrl; // URL
   } catch (error) {
-    console.error('Error following redirects:', error);
+    // console.error('Error following redirects:', error);
+    console.log("Error!")
+    console.log("domain", domain)
+    console.log("id", id)
   }
 }
 
@@ -47,10 +54,10 @@ async function submitForm(finalUrl, formData) {
 }
 
 // Main function to automate the entire process
-async function automizeProcess(formData, domain, pageUrl) {
+async function automizeProcess(formData, domain, pageUrl, extraPage) {
   try {
     // Step 1: Get them ids
-    const id = await fetchAndExtractID(domain, pageUrl);
+    const id = await fetchAndExtractID(domain, pageUrl, extraPage);
     console.log('Played id:', id);
 
     // Step 2: Follow redirects
